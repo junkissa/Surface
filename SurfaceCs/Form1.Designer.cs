06b67c58d5f0c2feda7c9ef1b0818a1d5e89ff96
junkissa <mosbilous@gmail.com>
2015-06-26 18:44:36 +0300

    Surface of 2nd order
    x = a*cos(u)*cos(v) + b*sin(u)*sin(v);
    y = a*cos(u)*sin(v) - b*sin(u)*cos(v);
    z = c*sin(u);

---
+++ SurfaceCs/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace plane2
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Требуется переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Обязательный метод для поддержки конструктора - не изменяйте
+		/// содержимое данного метода при помощи редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.AnT = new Tao.Platform.Windows.SimpleOpenGlControl();
+			this.button1 = new System.Windows.Forms.Button();
+			this.trackBar2 = new System.Windows.Forms.TrackBar();
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.trackBar3 = new System.Windows.Forms.TrackBar();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// AnT
+			// 
+			this.AnT.AccumBits = ((byte)(0));
+			this.AnT.AutoCheckErrors = false;
+			this.AnT.AutoFinish = false;
+			this.AnT.AutoMakeCurrent = true;
+			this.AnT.AutoSwapBuffers = true;
+			this.AnT.BackColor = System.Drawing.Color.Black;
+			this.AnT.ColorBits = ((byte)(32));
+			this.AnT.DepthBits = ((byte)(16));
+			this.AnT.Location = new System.Drawing.Point(5, 8);
+			this.AnT.Name = "AnT";
+			this.AnT.Size = new System.Drawing.Size(520, 398);
+			this.AnT.StencilBits = ((byte)(0));
+			this.AnT.TabIndex = 0;
+			this.AnT.Load += new System.EventHandler(this.AnT_Load);
+			this.AnT.KeyUp += new System.Windows.Forms.KeyEventHandler(this.AnT_KeyUp);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(534, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(201, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Намалювати";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// trackBar2
+			// 
+			this.trackBar2.Location = new System.Drawing.Point(608, 62);
+			this.trackBar2.Maximum = 100;
+			this.trackBar2.Name = "trackBar2";
+			this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+			this.trackBar2.Size = new System.Drawing.Size(45, 325);
+			this.trackBar2.TabIndex = 3;
+			this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.Location = new System.Drawing.Point(531, 62);
+			this.trackBar1.Maximum = 100;
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+			this.trackBar1.Size = new System.Drawing.Size(45, 325);
+			this.trackBar1.TabIndex = 2;
+			this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(531, 46);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(13, 13);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "a";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(605, 46);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(13, 13);
+			this.label2.TabIndex = 5;
+			this.label2.Text = "b";
+			// 
+			// trackBar3
+			// 
+			this.trackBar3.Location = new System.Drawing.Point(690, 62);
+			this.trackBar3.Maximum = 100;
+			this.trackBar3.Name = "trackBar3";
+			this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
+			this.trackBar3.Size = new System.Drawing.Size(45, 325);
+			this.trackBar3.TabIndex = 6;
+			this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(687, 46);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(13, 13);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "c";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(696, 388);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(13, 13);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "0";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(615, 388);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(13, 13);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "0";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(531, 388);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(13, 13);
+			this.label6.TabIndex = 10;
+			this.label6.Text = "0";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(823, 410);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.trackBar3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.trackBar2);
+			this.Controls.Add(this.trackBar1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.AnT);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private Tao.Platform.Windows.SimpleOpenGlControl AnT;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TrackBar trackBar2;
+		private System.Windows.Forms.TrackBar trackBar1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TrackBar trackBar3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+	}
+}
+
---
+++ SurfaceCs/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.OpenGl;
+using Tao.Platform.Windows;
+using Tao.FreeGlut;
+
+namespace plane2
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			AnT.InitializeContexts();
+		}
+
+		private void AnT_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			// инициализация Glut 
+			Glut.glutInit();
+			Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+
+			// очитка окна 
+			Gl.glClearColor(255, 255, 255, 1);
+
+			// установка порта вывода в соотвествии с размерами элемента anT 
+			Gl.glViewport(0, 0, AnT.Width, AnT.Height);
+
+
+			// настройка проекции 
+			Gl.glMatrixMode(Gl.GL_PROJECTION);
+			Gl.glLoadIdentity();
+			Glu.gluPerspective(45, (float)AnT.Width / (float)AnT.Height, 0.1, 200);
+			Gl.glMatrixMode(Gl.GL_MODELVIEW);
+			Gl.glLoadIdentity();
+
+			// настройка параметров OpenGL для визуализации 
+			Gl.glEnable(Gl.GL_DEPTH_TEST);
+ 
+		}
+		static double a = 1, b = 1, c = 1;
+		void Draw()
+		{
+
+			Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+
+			Gl.glLoadIdentity();
+			Gl.glColor3d(205.0/255.0, 92.0/255.0, 92.0/255.0);
+
+			Gl.glPushMatrix();
+			Gl.glTranslated(0, 0, -c * 3 - a/2 - b/2);
+			Gl.glRotated(90 + Zz, 1, 0, 0);
+			Gl.glRotated(Xx, 0, 0, 1);
+			double x = 0, y = 0, z = 0;
+			double step_big = 0.1, step_lit = 0.03;
+			
+			Gl.glPolygonMode(Gl.GL_FRONT_AND_BACK, Gl.GL_LINE);
+			for (double u = 0; u < 2 * Math.PI; u += step_big)
+			{
+				Gl.glBegin(Gl.GL_POLYGON);
+				for (double v = 0; v < 2 * Math.PI; v+=step_lit)
+				{
+					x = a*Math.Cos(u)*Math.Cos(v) + b*Math.Sin(u)*Math.Sin(v);
+					y = a*Math.Cos(u)*Math.Sin(v) - b*Math.Sin(u)*Math.Cos(v);
+					z = c*Math.Sin(u);
+					Gl.glVertex3d(x, y, z);
+				}
+				Gl.glEnd();
+			}
+			for (double v = 0; v < 2 * Math.PI; v += step_big)
+			{
+				Gl.glBegin(Gl.GL_POLYGON);
+				for (double u = 0; u < 2 * Math.PI; u+=step_lit)
+				{
+					x = a*Math.Cos(u)*Math.Cos(v) + b*Math.Sin(u)*Math.Sin(v);
+					y = a*Math.Cos(u)*Math.Sin(v) - b*Math.Sin(u)*Math.Cos(v);
+					z = c*Math.Sin(u);
+					Gl.glVertex3d(x, y, z);
+				}
+				Gl.glEnd();
+			}
+
+				Gl.glPopMatrix();
+			Gl.glFlush();
+			AnT.Invalidate();
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Draw();
+		}
+
+		private void trackBar1_Scroll(object sender, EventArgs e)
+		{
+			a = (double)trackBar1.Value/10.0;
+			label6.Text = a.ToString();
+			Draw();
+		}
+
+		private void trackBar2_Scroll(object sender, EventArgs e)
+		{
+			b = (double)trackBar2.Value/10.0;
+			label5.Text = b.ToString();
+			Draw();
+		}
+
+		private void trackBar3_Scroll(object sender, EventArgs e)
+		{
+			c = (double)trackBar3.Value/10.0;
+			label4.Text = c.ToString();
+			Draw();
+		}
+
+		double Xx = 0;
+		double Zz = 0;
+		double rotstep = 5;
+
+		private void AnT_KeyDown(object sender, KeyEventArgs e)
+		{
+
+			if (e.KeyCode == Keys.W)
+			{
+				Gl.glPushMatrix();
+				Gl.glRotated(rotstep + 90, 0, 0, 1);
+				Zz += rotstep;
+				Draw();
+				Gl.glPopMatrix();
+				Gl.glEnd();
+			}
+			if (e.KeyCode == Keys.Down)
+			{
+				Zz -= rotstep;
+				Draw();
+			}
+			if (e.KeyCode == Keys.Left)
+			{
+				Xx += rotstep;
+				Draw();
+			}
+			if (e.KeyCode == Keys.Right)
+			{
+				Xx -= rotstep;
+				Draw();
+			}
+		}
+
+		
+
+		private void AnT_KeyUp(object sender, KeyEventArgs e)
+		{
+				if (e.KeyCode == Keys.W)
+			{
+				Gl.glPushMatrix();
+				Gl.glRotated(rotstep + 90, 0, 0, 1);
+				Zz += rotstep;
+				Draw();
+				Gl.glPopMatrix();
+				Gl.glEnd();
+			}
+			if (e.KeyCode == Keys.S)
+			{
+				Zz -= rotstep;
+				Draw();
+			}
+			if (e.KeyCode == Keys.A)
+			{
+				Xx += rotstep;
+				Draw();
+			}
+			if (e.KeyCode == Keys.D)
+			{
+				Xx -= rotstep;
+				Draw();
+			}
+		}
+		
+		
+
+	}
+}
